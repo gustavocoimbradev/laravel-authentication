ede9a31c447d61ee124f7d5e749be6fb5d631665
Gustavo Lima <whoisgustavolima@gmail.com>
2025-05-21 21:49:44 -0300

    laravel-authentication

---
+++ resources/views/components/youtube-video.blade.php
@@ -0,0 +1,8 @@
+<iframe 
+  width="100%" 
+  height="240" 
+  src="{{ $url }}" 
+  frameborder="0" 
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+  allowfullscreen>
+</iframe>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,6 +5,7 @@
 @section('content')
     <x-box>
         <x-slot name="header">Hello, {{ $user->name }}!</x-slot>
+        <p class="text-center">Congratulations, you've successfully logged in! <a target="_blank" class="underline text-blue-500 font-semibold" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Click here</a> to claim your prize.</p>
         <x-form action="/logout" method="GET">
             <x-button variant="red" type="submit">Logout</x-button>
         </x-form>
