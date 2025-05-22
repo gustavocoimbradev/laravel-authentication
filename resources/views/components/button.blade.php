36d2852f5d627cd1b4bad8091bdc6611995a9509
Gustavo Lima <whoisgustavolima@gmail.com>
2025-05-21 21:23:57 -0300

    laravel-authentication

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,10 @@
 @php
-    $variantClasses = 'bg-'.$variant.'-500 hover:bg-'.$variant.'-600';
+    $variants = [
+        'blue' => 'bg-blue-500 hover:bg-blue-600',
+        'red'  => 'bg-red-500 hover:bg-red-600',
+    ];
+
+    $variantClasses = $variants[$variant] ?? $variants['blue'];
 @endphp
 
 <button class="px-5 py-2 cursor-pointer rounded-full mx-auto mt-6 block max-w-[max-content] transition-all ease-in-out duration-300 text-white {{ $variantClasses }}" type="{{ $type ?? 'button' }}">
